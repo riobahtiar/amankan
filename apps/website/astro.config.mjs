d7ad7dc44abbf3f430d991c23b2254e77fe81c07
Rio Bahtiar <rio.bahtiar@sekolahmu.co.id>
2023-01-15 08:25:00 +0700

    add amplitudo tracking code 2

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -13,12 +13,12 @@ import vercel from "@astrojs/vercel/serverless";
 import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
-import compress from "astro-compress";
+// import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://amankan.org',
-  integrations: [mdx(), sitemap(), image(), tailwind(), react(), prefetch(), compress()],
+  integrations: [mdx(), sitemap(), image(), tailwind(), react(), prefetch()],
   output: "server",
   adapter: vercel()
 });
